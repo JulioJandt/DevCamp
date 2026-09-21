f27fd4253397d9e27b23fc33b7e290167d0043cf
HeitorKaufmann <heitorskauf@gmail.com>
2026-09-21 19:01:42 -0300

    corrigido rotas

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 import express from 'express';
 import userRoutes from './src/routes/userRoutes.js';
 import teamRoutes from './src/routes/teamRoutes.js';
-const app = express();
+import matchRoutes from './src/routes/matchRoutes.js';
+import tournamentRoutes from './src/routes/tournamentRoutes.js';
 
+const app = express();
 const port = 3000;
+
 // Middleware para parse de JSON
 app.use(express.json());
+
 // Rotas
 app.use('/users', userRoutes);
 app.use('/teams', teamRoutes);
+app.use('/matches', matchRoutes); 
+app.use('/tournaments', tournamentRoutes); 
+
 // Rota inicial
 app.get('/', (req, res) => {
-res.send('API com Express funcionando!');
+  res.send('API com Express funcionando!');
 });
+
 app.listen(port, () => {
-console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
